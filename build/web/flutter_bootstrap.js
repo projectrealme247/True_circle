71878b67ad282bd397d478637b1349c85bb15a6b
projectrealme247 <projectrealme247@gmail.com>
2026-06-27 02:58:06 +0100

    fix: hardcode dart-defines for database initialization pipeline

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2990831394" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "773903173" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
